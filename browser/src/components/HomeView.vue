0ec87d83ef845b6a1d09e2b7c5d9459a7ab3ca66
David van IJzendoorn <vanijzen@DN0a24f31b.SUNet>
2023-08-08 13:50:20 -0700

    fix text viewer

--- browser/src/components/HomeView.vue
+++ browser/src/components/HomeView.vue
@@ -1,16 +1,17 @@
 <template>
     <div>
       <h1>About</h1>
-      <p>This IF Viewer enables swift and intuitive exploration of images acquired for the Spatial Macrophage Map Project, offering an invaluable visual resource for comprehending the scope of its findings.</p>
+      <p>This IF Viewer enables swift and intuitive exploration of images acquired for the Spatial Macrophage Map Project, 
+        offering an invaluable visual resource for comprehending the scope of its findings.</p>
       
       <h1>User Guide</h1>
         <ol>
-          <li>To navigate through this resource, use the two drop down menus to select and render an image.</li>
-          <li>Use your mouse or trackpad for high resolution deep-zoom in and intuitive navigation.</li>
-          <li>Legend listing stained protein names is located on the bottom left corner.</li>
-          <li>Images can be downloaded by clicking the Download slide button on the bottom left corner.</li>
-          <li>To share an image, click on the Current slide URL and copy link address.</li>
-          <li>Additional images and tabular data used to construct the Spatial Human Macrophage Map can be accessed under STUDY FILES button on the top right corner.</li>
+          <li>1. To navigate through this resource, use the two drop down menus to select and render an image.</li>
+          <li>2. Use your mouse or trackpad for high resolution deep-zoom in and intuitive navigation.</li>
+          <li>3. Legend listing stained protein names is located on the bottom left corner.</li>
+          <li>4. Images can be downloaded by clicking the Download slide button on the bottom left corner.</li>
+          <li>5. To share an image, click on the Current slide URL and copy link address.</li>
+          <li>6. Additional images and tabular data used to construct the Spatial Human Macrophage Map can be accessed under STUDY FILES button on the top right corner.</li>
         </ol>
 
   
