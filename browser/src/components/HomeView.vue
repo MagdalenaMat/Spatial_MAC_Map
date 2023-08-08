e76ac00aaad28ae6cf781a7be48016ac38d276f3
David van IJzendoorn <vanijzen@DN0a24f31b.SUNet>
2023-08-08 13:47:58 -0700

    fix text viewer

--- browser/src/components/HomeView.vue
+++ browser/src/components/HomeView.vue
@@ -1,19 +1,18 @@
 <template>
     <div>
       <h1>About</h1>
-      <p>
-        Recent advances in multiplex imaging offer unprecedented insight into understanding spatial tissue biology. However, handling big imaging files presents significant challenges, necessitating substantial computational power and coding expertise, thereby limiting accessibility primarily to domain experts. To surmount barriers of entry for non-experts and foster widespread accessibility of images acquired in this work, we have developed a cloud-based image viewer that operates effortlessly in web browsers. By accepting standard format tiff images and employing efficient pre-processing, the IF Viewer enables swift and intuitive exploration for researchers and non-experts alike, offering an invaluable visual resource for comprehending the scope of our findings.
-      </p>
+      <p>This IF Viewer enables swift and intuitive exploration of images acquired for the Spatial Macrophage Map Project, offering an invaluable visual resource for comprehending the scope of its findings.</p>
       
       <h1>User Guide</h1>
-      <p>
-        To navigate through this resource, use the two drop down menus to select and render an image.
-        Use your mouse or trackpad for high resolution deep-zoom in and intuitive navigation.
-        Legend listing stained protein names is located on the bottom left corner.
-        Images can be downloaded by clicking the Download slide button on the bottom left corner.
-        To share an image, click on the Current slide URL and copy link address.
-        Additional images and tabular data used to construct the Spatial Human Macrophage Map can be accessed under STUDY FILES button on the top right corner.
-      </p>
+        <ol>
+          <li>To navigate through this resource, use the two drop down menus to select and render an image.</li>
+          <li>Use your mouse or trackpad for high resolution deep-zoom in and intuitive navigation.</li>
+          <li>Legend listing stained protein names is located on the bottom left corner.</li>
+          <li>Images can be downloaded by clicking the Download slide button on the bottom left corner.</li>
+          <li>To share an image, click on the Current slide URL and copy link address.</li>
+          <li>Additional images and tabular data used to construct the Spatial Human Macrophage Map can be accessed under STUDY FILES button on the top right corner.</li>
+        </ol>
+
   
       <h1>Credit</h1>
       <p>
